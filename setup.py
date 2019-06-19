060e63d934821a83f2b1d211e8a9430de509b5fd
Scott Clark <scottclark591@gmail.com>
2019-06-19 11:02:52 -0400

    Bump version to 0.0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="psycho",
-    version="0.0.7",
+    version="0.0.8",
     description="An ultra simple wrapper for Python psycopg2 with very basic functionality",
     author="Scott Clark",
     author_email="scott@usealloy.io",
